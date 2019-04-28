f5291135bf17beb19d9eb920ebd10094c4b69c1f
deepikawebdev <33912886+deepikawebdev@users.noreply.github.com>
2019-04-27 21:11:51 -0700

    first upload
    Basic compilation of the application where route
    / : displays hello world.
    /math:displays a page to accept the inputs from the user
    /auth: for authentication with username:testUser and password=password1234

---
+++ app.js
@@ -0,0 +1,88 @@
+
+
+const fs = require('fs');
+var koa = require('koa');
+const Router=require('koa-router');
+
+const bodyParser=require('koa-parser');
+var app = new koa();
+const port=3000;
+var credentials = { name: 'testUser', pass: 'password1234' }
+var auth = require('koa-basic-auth');
+const router=new Router();
+//challenge Part-1 to display: hello world for route /
+const koaBody = require('koa-body');
+
+app.use(bodyParser());
+
+
+
+router.get('/',(ctx)=>{
+    ctx.body = `hello world!.`;
+});
+
+router.get('/auth', auth(credentials) , async (ctx) =>{
+    
+    ctx.body = 'You have access to the protected area.';
+    
+ });
+ 
+
+app.use(function *(next){
+    try {
+       yield next;
+    } catch (err) {
+       if (401 == err.status) {
+          this.status = 401;
+          this.set('WWW-Authenticate', 'Basic');
+          this.body = 'You have no access here';
+       } else {
+          throw err;
+       }
+    }
+ });
+ 
+router.get('/math', async (ctx) => {
+    ctx.type = 'html';
+    ctx.body = fs.createReadStream(`./index.html`);
+});
+router.post('/post', ctx=>{
+    let p=(ctx.request.body);
+    var res=0;
+    let one=parseInt(p.ip1);
+    let two=parseInt(p.ip2);
+    let op=p.op;
+    console.log(p);
+    if(isNaN(one) ||  isNaN(two)){
+        ctx.body="Please provide a value in each of the fields!";
+    }
+    else{
+    switch(op){
+        case "+" :  res=one+two;
+                    //console.log( res);
+                    break;
+    
+        case "-" : 
+                    res=(one-two);
+                    //console.log( res);
+                    
+                    break;
+                   
+       case "*" :  res=one*two;
+                    //console.log( res);
+                    break;
+                     
+        case "/" :  res=one/two;
+                    //console.log( res);
+                    break;
+                    
+                }
+                
+    ctx.body=` The result from your performed operation is : ${res}`;
+}
+})
+
+app.use(router.routes());
+app.listen(port, function(){
+    console.log(`Server is listening on port http://localhost:${port}`);
+})
