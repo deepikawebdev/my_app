a819e209bfa1d474a6f1cda90073363ec40e7cba
deepikawebdev <33912886+deepikawebdev@users.noreply.github.com>
2019-04-27 22:03:01 -0700

    Update app.js
    additional comments

--- app.js
+++ app.js
@@ -7,40 +7,21 @@ const Router=require('koa-router');
 const bodyParser=require('koa-parser');
 var app = new koa();
 const port=3000;
-var credentials = { name: 'testUser', pass: 'password1234' }
+var credentials = { name: 'testUser', pass: 'password1234' };
 var auth = require('koa-basic-auth');
 const router=new Router();
-//challenge Part-1 to display: hello world for route /
 const koaBody = require('koa-body');
 
 app.use(bodyParser());
 
 
-
+//challenge Part-1 to display: hello world for route /
 router.get('/',(ctx)=>{
     ctx.body = `hello world!.`;
 });
 
-router.get('/auth', auth(credentials) , async (ctx) =>{
-    
-    ctx.body = 'You have access to the protected area.';
-    
- });
- 
 
-app.use(function *(next){
-    try {
-       yield next;
-    } catch (err) {
-       if (401 == err.status) {
-          this.status = 401;
-          this.set('WWW-Authenticate', 'Basic');
-          this.body = 'You have no access here';
-       } else {
-          throw err;
-       }
-    }
- });
+//challenge Part-2 /math route to get input from the user
  
 router.get('/math', async (ctx) => {
     ctx.type = 'html';
@@ -81,6 +62,28 @@ router.post('/post', ctx=>{
     ctx.body=` The result from your performed operation is : ${res}`;
 }
 })
+
+//challenge Part-3 /auth route to authenticate user 
+router.get('/auth', auth(credentials) , async (ctx) =>{
+    
+    ctx.body = 'You have access to the protected area.';
+    
+ });
+ 
+
+app.use(function *(next){
+    try {
+       yield next;
+    } catch (err) {
+       if (401 == err.status) {
+          this.status = 401;
+          this.set('WWW-Authenticate', 'Basic');
+          this.body = 'You have no access here';
+       } else {
+          throw err;
+       }
+    }
+ });
 
 app.use(router.routes());
 app.listen(port, function(){
